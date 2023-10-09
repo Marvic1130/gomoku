6b0a926cbe3419899318d59f51de65b9d8116b1a
Marvic1130 <marvic1130@gmail.com>
2023-10-10 01:04:56 +0900

    Create Board.py

---
+++ Rules/Board.py
@@ -0,0 +1,54 @@
+from enum import Enum
+import Rules
+
+class Stone(Enum):
+    B = 1
+    W = 2
+
+    def __str__(self):
+        return 'Black' if self is Stone.B else 'White'
+
+class Board:
+    def __init__(self, size=15):
+        self.size = size
+        self._board = [[None for _ in range(size)] for _ in range(size)]
+        self.turn = 0
+
+    @property
+    def board(self):
+        return self._board
+
+    def get(self, row, col):
+        return self._board[row][col]
+
+    def is_valid(self, row, col, stone):
+        return Rules.check_violation(self._board, stone, row, col)
+
+    def is_empty(self, row, col):
+        return self._board[row][col] is None
+
+    def is_full(self):
+        return self.turn == self.size ** 2
+
+    def is_win(self, row, col, stone):
+        return Rules.check_win(self._board, row, col, stone)
+
+    def set(self, row, col, stone):
+        if self._board[row][col] is not None:
+            return False, None
+        elif stone is Stone.B:
+            if self.is_valid(row, col, stone):
+                self._board[row][col] = stone
+                self.turn += 1
+            else:
+                return False, None
+
+        elif stone is Stone.W:
+            self._board[row][col] = stone
+            self.turn += 1
+
+        win = self.is_win(row, col, stone)
+        if win:
+            return True, str(stone)
+        elif self.is_full():
+            return True,  'Draw'
