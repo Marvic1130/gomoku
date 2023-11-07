62cbde337a09a37636550fb16999e1759382449c
Marvic1130 <marvic1130@gmail.com>
2023-11-07 22:07:12 +0900

    First Model & Training
    ResNet Training

---
+++ Train/Model.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+
+
+class ResNetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super(ResNetBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=2)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.LeakyReLU(0.1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride, padding=2)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        identity = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += identity
+        out = self.relu2(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(ResNet, self).__init__()
+        self.conv = nn.Conv2d(in_channels, 64, kernel_size=5, stride=1, padding=2)
+        self.bn = nn.BatchNorm2d(64)
+        self.relu = nn.LeakyReLU(0.1)
+        self.blocks = nn.ModuleList([ResNetBlock(64, 64, kernel_size=5, stride=1) for _ in range(10)])
+        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64, num_classes)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        for block in self.blocks:
+            out = block(out)
+        out = self.global_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+class BiLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(BiLSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size * 2, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(x.device)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])  # 마지막 시퀀스의 출력만 사용
+        return out
+
+
+class DQN(nn.Module):
+    def __init__(self, input_size, num_actions):
+        super(DQN, self).__init__()
+        self.dense1 = nn.Linear(input_size, 64)
+        self.relu1 = nn.ReLU()
+        self.dense2 = nn.Linear(64, 32)
+        self.relu2 = nn.ReLU()
+        self.output_layer = nn.Linear(32, num_actions)
+
+    def forward(self, inputs):
+        x = self.relu1(self.dense1(inputs))
+        x = self.relu2(self.dense2(x))
+        return self.output_layer(x)
+
+
+# BiLSTM 모델 빌드
+input_size = 15 * 15
+hidden_size = 128  # LSTM hidden state 크기
+num_layers = 1  # LSTM 레이어 수
+num_classes = 15 * 15  # 가능한 착수 위치의 수
+bilstm_model = BiLSTM(input_size, hidden_size, num_layers, num_classes)
+
+# 모델 요약
+print(bilstm_model)
+
+
+# 모델 생성
+in_channels = 2  # 입력 이미지 채널 수
+num_classes = 2  # 클래스 수 (예: 게임 결과 - 승, 패)
+resnet_model = ResNet(in_channels, num_classes)
+
+# 모델 요약
+print(resnet_model)
--- Train/train.py
+++ Train/train.py
@@ -1,62 +1,140 @@
-import numpy as np
+from time import sleep
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+from Model import ResNet
 import pandas as pd
+import numpy as np
 
-def convert_moves(moves):
-    board = np.zeros((board_size, board_size), dtype=int)
-    turn = np.full((board_size, board_size), -1, dtype=int)
-
-    for i, move in enumerate(moves):
-        col, row = ord(move[0]) - ord('a'), int(move[1:]) - 1  # 이동을 좌표로 변환
-        if board[row, col] == 0:  # 해당 위치에 돌이 없는 경우에만 놓기
-            if i % 2 == 0:
-                board[row, col] = 1  # 홀수 번째 수에는 흑돌을 나타내는 1을 해당 위치에 설정
-            else:
-                board[row, col] = -1  # 짝수 번째 수에는 백돌을 나타내는 -1을 해당 위치에 설정
-            turn[row, col] = i  # 해당 위치에 착수 순서 저장
-        else:
-            raise ValueError("This location already exists.")
-
-    # 상하반전 적용
-    board = np.flipud(board)
-    turn = np.flipud(turn)
-
-    return board, turn
-
-csv_file_path = '../Data/gameData.csv'
+# 데이터 로드 및 전처리
+csv_file_path = '../Data/gameData.csv'  # 데이터 파일 경로
 df = pd.read_csv(csv_file_path)
-
-# 보드 크기 (15x15) 정의
 board_size = 15
+x = []  # 보드 상태를 저장할 목록
+y = []  # 대상 움직임을 저장할 목록
 
-# CSV 데이터 처리
-augmented_data = []
+
+def move_to_label(move, board_size):
+    col, row = ord(move[0]) - ord('a'), int(move[1:]) - 1
+    return row * board_size + col
+
 
 for index, row in df.iterrows():
-    moves = row['moves'].split()  # 움직임 데이터를 리스트로 변환
-    result = row['bresult']  # 게임 결과 (0 또는 1)
-    # 게임 보드 초기화
-    board, turn = convert_moves(moves)
+    moves = row['moves'].split()  # 움직임 데이터를 목록으로 분할
+    result = row['bresult']  # 게임 결과 (0, 0.5, 또는 1)
 
-    for i in range(4):  # 회전과 반전을 고려한 데이터 확장
-        rotated_board = np.rot90(board, k=i)
-        rotated_turn = np.rot90(turn, k=i)
-        augmented_data.append([rotated_board, rotated_turn, result])
+    board = np.zeros((board_size, board_size), dtype=int)  # 게임 보드 초기화
+    game_x = []  # 이 게임의 보드 상태를 저장할 목록
+    game_y = []  # 이 게임의 대상 움직임을 저장할 목록
 
-        flipped_board = np.fliplr(rotated_board)
-        flipped_turn = np.fliplr(rotated_turn)
-        augmented_data.append([flipped_board, flipped_turn, result])
+    for i, move in enumerate(moves):
+        # 움직임 데이터 변환
+        label = move_to_label(move, board_size)
 
-# 데이터를 NumPy 배열로 변환
-augmented_data = np.array(augmented_data, dtype=object)
+        # 게임 결과를 확인하고 데이터를 이에 따라 수집합니다.
+        if result == 0 and i % 2 == 0:
+            game_x.append(list(board))  # 현재 보드 상태를 추가
+            game_y.append(label)  # 변환된 움직임을 추가
+        if result == 0.5:
+            game_x.append(list(board))  # 현재 보드 상태를 추가
+            game_y.append(label)  # 변환된 움직임을 추가
+        if result == 1 and i % 2 == 1:
+            game_x.append(list(board))  # 현재 보드 상태를 추가
+            game_y.append(label)  # 변환된 움직임을 추가
 
-# 데이터와 레이블로 분리
-x = np.array(augmented_data[:, :2].tolist(), dtype=np.float32)
-y = np.array(augmented_data[:, 2], dtype=np.float32)
+        col, row = ord(move[0]) - ord('a'), int(move[1:]) - 1  # 움직임을 좌표로 변환
 
-print(x.shape)  # (N*8, 15, 15, 2)
-print(y.shape)  # (N*8,)
+        if board[row, col] == 0:
+            if i % 2 == 0:
+                board[row, col] = 1
+            else:
+                board[row, col] = -1
+        else:
+            raise ValueError("이 위치에 이미 돌이 있습니다.")
 
-print("첫 번째 데이터:")
-print(x[0])
-print("첫 번째 레이블:")
-print(y[0])
+    # 게임 데이터를 주 x 및 y 목록에 추가합니다.
+    x.extend(game_x)
+    y.extend(game_y)
+
+# 데이터셋 및 데이터로더 설정
+class CustomDataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        x = torch.tensor(np.array(self.data[idx]), dtype=torch.float32).unsqueeze(0)  # 채널 차원 추가
+        y = torch.tensor(self.labels[idx], dtype=torch.long)
+        return x, y
+
+# 데이터를 학습 및 검증 세트로 분할
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
+
+train_dataset = CustomDataset(x_train, y_train)
+val_dataset = CustomDataset(x_val, y_val)
+
+batch_size = 128
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+# ResNet 모델 빌드
+in_channels = 1  # 입력 이미지 채널 수
+num_classes = 15 * 15  # 클래스 수 (착수 위치 수)
+resnet_model = ResNet(in_channels, num_classes)
+
+# 모델 요약
+print(resnet_model)
+
+# 모델을 선택한 장치로 이동 (CPU 또는 GPU)
+device = torch.device("cuda" if torch.cuda.is_available() else 'mps:0' if torch.backends.mps.is_available() else 'cpu')
+resnet_model.to(device)
+
+# 손실 함수 및 옵티마이저 설정
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(resnet_model.parameters(), lr=0.001)
+
+# 학습 루프
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    resnet_model.train()
+    total_loss = 0.0
+
+    for inputs, labels in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}"):
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = resnet_model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    avg_loss = total_loss / len(train_loader)
+    print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {avg_loss:.4f}")
+    sleep(1)
+
+# 평가
+resnet_model.eval()
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for inputs, labels in val_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = resnet_model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+accuracy = 100 * correct / total
+print(f"Validation Accuracy: {accuracy:.2f}%")
